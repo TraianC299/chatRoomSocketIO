b8992ed2f52b62ceaec4b917c2a8ba1c92ffa3f9
TraianC299 <traianciochina@gmail.com>
2022-09-27 17:17:55 +0300

    bingo

--- public/js/main.js
+++ public/js/main.js
@@ -7,47 +7,35 @@ const userList = document.getElementById('users');
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
-
 const socket = io();
 
-// Join chatroom
+
+socket.on("message", (message) => {
+  outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+
+});
+
+
+//join chat room
 socket.emit('joinRoom', { username, room });
 
-// Get room and users
+
+//messag esubmit
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const msg = e.target.elements.msg.value;
+  socket.emit('chatMessage', msg);
+  e.target.elements.msg.value = '';
+});
+
 socket.on('roomUsers', ({ room, users }) => {
   outputRoomName(room);
   outputUsers(users);
 });
 
-// Message from server
-socket.on('message', (message) => {
-  console.log(message);
-  outputMessage(message);
 
-  // Scroll down
-  chatMessages.scrollTop = chatMessages.scrollHeight;
-});
 
-// Message submit
-chatForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  // Get message text
-  let msg = e.target.elements.msg.value;
-
-  msg = msg.trim();
-
-  if (!msg) {
-    return false;
-  }
-
-  // Emit message to server
-  socket.emit('chatMessage', msg);
-
-  // Clear input
-  e.target.elements.msg.value = '';
-  e.target.elements.msg.focus();
-});
 
 // Output message to DOM
 function outputMessage(message) {
--- utils/users.js
+++ utils/users.js
@@ -28,6 +28,8 @@ function getRoomUsers(room) {
   return users.filter(user => user.room === room);
 }
 
+
+
 module.exports = {
   userJoin,
   getCurrentUser,
